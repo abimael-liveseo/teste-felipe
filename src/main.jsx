6e7e2e7f45e27543c060dafe4961fa43103638f3
Filipe Oliveira <filipeoliveira0426@gmail.com>
2023-05-06 15:19:43 -0300

    Hero

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Hero from "./components/Hero";
+import ServiceDown from './components/ServiceDown'
 
-import './index.css'
+import "./style.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<Hero />
+		<ServiceDown/>		
+	</React.StrictMode>
+);
